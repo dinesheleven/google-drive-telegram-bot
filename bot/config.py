817fafc164774195b3b63fa668d809c855cb52d7
dinesheleven <dinesh11.1997@gmail.com>
2021-08-05 13:46:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,12 +2,12 @@ class config:
     BOT_TOKEN = "1945378962:AAHzQ7VB73G2ba0j5o91pPpW-cA2V-8h0Q8"
     APP_ID = "5221390"
     API_HASH = "81905a948f1801130e284086ac88f365"
-    DATABASE_URL = "postgres://zyoroejjxwnmqz:925de386fc40f9487370e2b95df6cf90e526f8345d60d72d0d0e8389a6af45eb@ec2-54-196-65-186.compute-1.amazonaws.com:5432/d6g2c6gc8qa5ff"
+    DATABASE_URL = "postgres://hkinmzyblxfgxe:a8422d50d114384c8c32a730199df1b2ecf70a1ab9037bf38394df9d95ba6da5@ec2-3-230-38-145.compute-1.amazonaws.com:5432/d38gugfujk2gss"
     SUDO_USERS = "820559046" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "1086361525077-hm9ard83c4uj1rc7ir2ecb7tefi8co2m.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "sg6RLP3AT_hsn7_fVZOLjyKa"
 
 
 class BotCommands:
